8cc5b6a11f3c49e9a193913fcbacb8fce342c3e5
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2022-05-12 16:46:12 -0500

    Second commit

--- newton.py
+++ newton.py
@@ -9,11 +9,11 @@ def f_derivada(x):
 def newton(x,n):
     xn = x - (f(x)/f_derivada(x))
     if xn != x:
-        print(f'n_{n} = {xn}')
+        print(f'x_{n} = {xn}')
         n = n + 1
         newton(xn,n) 
     else:
         return
 
 x0 = 1
-newton(x0,0)
+newton(x0,1)
